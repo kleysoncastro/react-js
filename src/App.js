262fbd6c3c587eb21083d297f54b71b1db5cfe4c
kleyson castro <kleysoncastro99@gmail.com>
2019-10-25 17:01:44 -0300

    chore: add file-loader para importar imagens

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css'
+import imgSand from './assets/sand.jpeg';
+
+function App() {
+  return <img src={imgSand} />
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,23 @@ module.exports = {
       exclude: /node_modules/,
       use: {
         loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'},
+
+      ]
+    },
+    {
+      test: /.*\.(gif|png|jpe?g)$/i,
+      use: {
+        loader: 'file-loader'
       }
-    }]
+    }
+  ]
   }
 
 }
